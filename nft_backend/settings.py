3f4ed083828a22982cb665ef21536cd9c96bf770
Ted McGinness <ttmcginness22@gmail.com>
2022-03-15 21:53:55 -0500

    heroku deeploy

--- nft_backend/settings.py
+++ nft_backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-import dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
