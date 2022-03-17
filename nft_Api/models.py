d9eae8cafdfea42fd44fc315e2a327e98543ee43
Ted McGinness <ttmcginness22@gmail.com>
2022-03-17 10:28:05 -0500

    please heroku i'm begging you

--- nft_Api/models.py
+++ nft_Api/models.py
@@ -6,11 +6,11 @@ from django.contrib.postgres.fields import ArrayField
 class NFT(models.Model):
     image = models.TextField(default="none")
     name = models.CharField(max_length=32)
-    price = models.IntegerField()
-    description = models.TextField()
+    price = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     offer = models.BooleanField(default=False)
     offer_price = models.IntegerField(default=0)
-    owner = models.CharField(max_length=64, default="None")
+    owner = models.CharField(max_length=64, blank=True, null=True)
     blockchain = models.CharField(max_length=16, default="Ethereum")
     collection = models.CharField(max_length=32, blank=True, null=True)
     properties = models.TextField(blank=True, null=True)
@@ -22,6 +22,10 @@ class User(models.Model):
     lname = models.CharField(max_length=32)
     password = models.CharField(max_length=50)
     collection = models.CharField(
+        max_length=200, blank=True, null=True)
+    created = models.CharField(
+        max_length=200, blank=True, null=True)
+    favorited = models.CharField(
         max_length=200, blank=True, null=True)
     offers = models.CharField(
         max_length=200, blank=True, null=True)
